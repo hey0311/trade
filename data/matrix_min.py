466d9616fae3377b500f615d3c05c96d0d8a9ab1
hey0311 <hey0311@qq.com>
2019-05-17 17:11:49 +0800

    微调

--- data/matrix_min.py
+++ data/matrix_min.py
@@ -11,35 +11,45 @@ import pandas as pd
 # token = 'jr5WbEOp' #13641294686 hey7520 用完
 # token = 'u2fPK9yR'  # 409620155@qq.com react123 用完
 # token = 'ajHewWnR' #hey0311@126.com redux123 用完
-token = '8ELP2rPy' #lvren1969@gmail.com angular123
+token = '8ELP2rPy'  # lvren1969@gmail.com angular123
 headers = {"Authorization": token, "Content-type": "application/json"}
-symbol = 'EOS'
+symbol = 'EOS/USD.OK.INDEX'
 interval = '1m'
+year = 2018
+month = 10
+day = 16
+hour = 0
+startDate = datetime.datetime(year, month, day, hour, 0, 0, 0)
+if interval == '1m':
+    timedelta = datetime.timedelta(hours=8)
+elif interval == '1d':
+    timedelta = datetime.timedelta(days=480)
 
-# startDate=datetime.datetime(2018, 10, 16, 0, 0, 0, 0)
-startDate = datetime.datetime(2019, 2, 14, 8, 0, 0, 0)
-# startDate = datetime.datetime(2019, 5, 13, 16, 0, 0, 0)
-endDate = startDate + datetime.timedelta(hours=8)
-stopDate = datetime.datetime(2019, 5, 15, 23, 0, 0, 946118)
+endDate = startDate + timedelta
+now = datetime.now()
 
-while startDate < stopDate:
-    requireStr = "https://api.matrixdata.io/matrixdata/api/v1/indexbarchart?symbol=" + symbol + "/USD.OK.INDEX&interval=" + interval + "&start=" + startDate.strftime(
-        "%Y-%m-%d %H:%S:%M") + "&end=" + endDate.strftime("%Y-%m-%d %H:%S:%M") + "&limit=500"
+while startDate < now:
+    requireStr = "https://api.matrixdata.io/matrixdata/api/v1/indexbarchart?symbol=" + \
+                 symbol + \
+                 "&interval=" + interval + \
+                 "&start=" + startDate.strftime("%Y-%m-%d %H:%S:%M") + \
+                 "&end=" + endDate.strftime("%Y-%m-%d %H:%S:%M") + \
+                 "&limit=500"
     print(requireStr)
     response = requests.get(requireStr, headers=headers)
     data = response.json()
 
     if data['Head']['Code'] == '200':
         df = pd.DataFrame.from_records(data['Result'])
-        filename = 'okex/'+symbol + '/'+interval + '_index/' + startDate.strftime("%Y-%m-%d(%H") + '-' + endDate.strftime(
-            "%H)") + '.csv'
+        filename = 'matrix_data/' + symbol + '/' + interval + '_index/' + \
+                   startDate.strftime("%Y-%m-%d(%H") + '-' + \
+                   endDate.strftime("%H)") + '.csv'
         print('请求成功!生成' + filename)
         df.to_csv(filename, index=False)
-        startDate = startDate + datetime.timedelta(hours=8)
-        endDate = endDate + datetime.timedelta(hours=8)
-        if stopDate < endDate:
-            endDate = stopDate
-        # time.sleep(50)
+        startDate = startDate + timedelta
+        endDate = endDate + timedelta
+        if now < endDate:
+            endDate = now
     else:
         print('请求失败!')
 print('程序已结束')
--- util/utils.py
+++ util/utils.py
@@ -11,3 +11,29 @@ def tran_mongo_2_df(items):
     data_employee_ri = data_employee.reindex(columns=['date', 'o', 'h','l','c','v'])
     return data_employee_ri
 
+#创建文件夹
+def mkdir(path):
+    # 引入模块
+    import os
+
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists=os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        print(path+' 创建成功')
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path+' 目录已存在')
+        return False
